1e5e187ed2dcc03f2598b763a250af8f872b56a6
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-09-11 10:27:30 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react"
-import Navbar from "./components/Navbar"
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
 import Landing from "./components/Landing";
 import P2Readme from "./components/readmes/P2Readme";
 
 
 function App() {
+  const emailAddress = "dimitar.vidolov@gmail.com"
   return (
     <div>
       <Router>
@@ -19,7 +19,7 @@ function App() {
       </Router>
 
       <footer>
-        <p>Reach me on <a className="" href="https://linkedin.com"> LinkedIn</a> or send me an <a className=" link--logo-image mail" href="mailto:dimitar.vidolov@gmail.com">email</a>.
+        <p>Reach me on <a className="" href="https://www.linkedin.com/in/dvidolov/"> LinkedIn</a> or send me an <a className=" link--logo-image mail" href={`mailto:${emailAddress}`}>email</a>.
         </p>
       </footer>
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,12 @@
-import { Link, NavLink } from "react-router-dom"
 import React from "react"
+import { Link, NavLink } from "react-router-dom"
 import './Navbar.css';
 
 
 function Navbar() {
+
+  const emailAddress = "dimitar.vidolov@gmail.com"
+
   return (
     <div>
       <h1>Dimitar Vidolov's portfolio</h1>
@@ -22,11 +25,11 @@ function Navbar() {
         </div>
 
         <div className="rightDiv">
-        <a className="navlink link--logo-image mail" href="mailto:dimitar.vidolov@gmail.com">✉️</a>
-          <a className="navlink" href="https://linkedin.com">
+        <a className="navlink link--logo-image mail" href={`mailto:${emailAddress}`}>✉️</a>
+          <a className="navlink" href="https://www.linkedin.com/in/dvidolov/">
             <img className="link--logo-image" alt="linkedin logo" src="https://brand.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" />
           </a>
-          <a className="navlink" href="https://github.com">
+          <a className="navlink" href="https://github.com/Dumblevor">
             <img className="link--logo-image" src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" alt="github logo" />
           </a>
         </div>

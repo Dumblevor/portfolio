ed8f2d889956fb8e4038dd0a958406891e2da3d5
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-09-23 14:22:59 +0300

    updates on styling for smaller screens

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,10 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { NavHashLink } from 'react-router-hash-link';
 import dvLogo from '../assets/logos/dv-logo.webp';
-
+import liLogo from "../assets/logos/LI-In-Bug.webp";
+import ghLogo from "../assets/logos/GitHub-Mark-64px.webp";
 
 function Navbar() {
 
@@ -12,12 +13,13 @@ function Navbar() {
       <h1>Dimitar Vidolov&apos;s portfolio</h1>
       <header className="headerStyle">
         <div className="leftDiv">
+          
           {/*<NavHashLink smooth className="navlink logo" to="/#section0"> &lt;D V&gt;*/}
+
           <a className="logo-wrapper" href="/#section0">
-
             <img className="logo" alt="Dimitar Vidolov logo" src={dvLogo} />
-
           </a>
+
           <NavHashLink smooth className="navlink textlink" to="#section2">
             Projects
           </NavHashLink>
@@ -26,10 +28,10 @@ function Navbar() {
         <div className="rightDiv">
           <a className="navlink link--logo-image mail" href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#100;&#105;&#109;&#105;&#116;&#97;&#114;&#46;&#118;&#105;&#100;&#111;&#108;&#111;&#118;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;">✉️</a>
           <a className="navlink" href="https://www.linkedin.com/in/dvidolov/" target="_blank" rel="noopener noreferrer">
-            <img className="link--logo-image" alt="linkedin logo" src="https://brand.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" />
+            <img className="link--logo-image" alt="linkedin logo" src={liLogo} />
           </a>
           <a className="navlink" href="https://github.com/Dumblevor" target="_blank" rel="noopener noreferrer">
-            <img className="link--logo-image" src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" alt="github logo" />
+            <img className="link--logo-image" src={ghLogo} alt="github logo" />
           </a>
         </div>
       </header>
